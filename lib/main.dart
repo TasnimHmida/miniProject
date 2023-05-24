b5aa35388d4832930eaecbf965a64b0ca5ca0771
Tasnim Hmida <tasnim.hmida@horizon-tech.tn>
2023-05-24 12:01:47 +0100

    worked on responsive

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
         home:
-        // showHome? const GetStartedScreen() :
+        showHome? const GetStartedScreen() :
         const OnBoardingView(),
       );
     });
